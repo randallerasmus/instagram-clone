3ec850fd93d781db0afe935200797ac0b716b855
randall.erasmus <randallerasmus1@gmail.com>
2021-10-21 23:17:48 +0200

    Adding tailwind forms to override defualt styles

--- components/Header.js
+++ components/Header.js
@@ -13,11 +13,11 @@ function Header(){
         <div>
             <div className="flex justify-between max-w-6xl">
                 {/*left*/}
-                <div className='relative hidden lg:inline-grid h-24 w-24 cursor-pointer'>
+                <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
                     <Image src="https://links.papareact.com/ocw"
                            layout="fill" objectFit="contain"   />
                 </div>
-                <div className="relative w-10 h-10 lg:hidden flex-shrink-0">
+                <div className="relative w-10 lg:hidden flex-shrink-0">
                     <Image src="https://links.papareact.com/jjm"
                            layout="fill" objectFit="contain"   />
 
@@ -25,13 +25,14 @@ function Header(){
                 </div>
 
                 {/*Middle - Search input field*/}
-                <div className="relative mt-1 p-9 rounded-md">
+                <div className="relative mt-1 p-3 rounded-md">
                     <div className="absolute inset-y-0 pl-3 flex items-center
                         pointer-events-none">
                         <SearchIcon className="h-5 w-5 text-gray-500"/>
                     </div>
 
-                    <input className="bg-gray-50 block w-full pl-10"
+                    <input className="bg-gray-50 block w-full pl-10 sm:text-sm
+                           border-gray focus:ring-black focus:border-black"
                            type="text"
                            placeholder="Search"/>
                 </div>
