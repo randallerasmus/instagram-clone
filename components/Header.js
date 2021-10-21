dc753604e752dea6fd19f857c55be0d899dd099a
randall.erasmus <randallerasmus1@gmail.com>
2021-10-21 21:27:28 +0200

    Setting up the header page

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+function Header(){
+    return (
+        <div>
+        <h1>I am header</h1>
+            {/*left*/}
+                <Image src="https://links.papareact.com/ocw"
+                    layout="fill"
+
+                />
+
+            {/*Middle*/}
+
+            {/*Right*/}
+
+        </div>
+
+
+
+    );
+}
+    export default Header
---
+++ next.config.js
@@ -0,0 +1,7 @@
+
+//When adding a new config file in next.js you must reload the server
+module.exports = {
+    images:{
+        domains: ["links.papareact.com"],
+    },
+};
