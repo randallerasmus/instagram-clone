e73b463f26b588323dc92c808e0d95df5af3f74a
randall.erasmus <randallerasmus1@gmail.com>
2021-10-22 08:18:23 +0200

    Make the header sticky

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,8 @@ import {HomeIcon} from "@heroicons/react/solid";
 
 function Header(){
     return (
-        <div>
+            // Adding a Nice and Sticky Header
+        <div className="shadow-sm border-b bg-white sticky top-0 z-50">
             <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
                 {/*left*/}
                 <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
