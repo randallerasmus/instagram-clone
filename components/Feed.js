f0b6689a81cb4108ddb56b25f26e1be77ef7283d
randall.erasmus <randallerasmus1@gmail.com>
2021-10-23 23:13:30 +0200

    Adding a Feed for the instagram

---
+++ components/Feed.js
@@ -0,0 +1,30 @@
+import Stories from "./Stories"
+
+function Feed () {
+    return (
+        // Planning out the grid
+        <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl
+               xl:grid-cols-3 xl:max-w-6xl max-auto">
+
+            {/*Section on the left*/}
+            <section className="col-span-2">
+                {/*Stories*/}
+                <Stories/>
+
+                {/*Post*/}
+            </section>
+
+
+
+
+            {/*Section*/}
+            {/*Mini Profile*/}
+            {/*Suggestions*/}
+
+
+
+        </main>
+    )
+}
+
+export default Feed
