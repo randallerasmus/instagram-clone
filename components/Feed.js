68cb5b7bf98619bfe85bcff0364e3c662064eb47
randall.erasmus <randallerasmus1@gmail.com>
2021-10-25 01:11:30 +0200

    fixing the story style to the middle

--- components/Feed.js
+++ components/Feed.js
@@ -4,7 +4,7 @@ function Feed () {
     return (
         // Planning out the grid
         <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl
-               xl:grid-cols-3 xl:max-w-6xl max-auto">
+               xl:grid-cols-3 xl:max-w-6xl mx-auto">
 
             {/*Section on the left*/}
             <section className="col-span-2">
