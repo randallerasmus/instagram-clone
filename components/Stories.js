b1bb2b870790d549c99bcb253791554d04576937
randall.erasmus <randallerasmus1@gmail.com>
2021-10-25 01:09:38 +0200

    styling the instagram stories scrollbar hide and the thumb black scrollbar

--- components/Stories.js
+++ components/Stories.js
@@ -26,9 +26,9 @@ function Stories(){
     },  []);
 
     return (
-    <div className='flex space-x-2 p-6
+    <div className="flex space-x-2 p-6
             bg-white mt-8 border-gray-200 border rounded-sm
-            overflow-x-scroll scrollbar-thin scrollbar-thumb-black '   >
+            overflow-x-scroll scrollbar-thin scrollbar-thumb-black"   >
         {/*So now here we will add loads of stoies*/}
 
                 {suggestions.map((profile) => (
