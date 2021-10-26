c9e2fa1544ec5b1d20eb82f6163d888ef38fdaa8
randall.erasmus <randallerasmus1@gmail.com>
2021-10-26 23:49:43 +0200

    Starting with the Post - single post

--- components/Post.js
+++ components/Post.js
@@ -1,8 +1,32 @@
+import {
+    SearchIcon,
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+    MenuIcon, DotsHorizontalIcon,
+} from "@heroicons/react/outline";
 
-function Post(){
+import { HeartIcon as HeartIconFilled} from "@heroicons/react/solid";
+
+function Post(id,username,userImg, img,caption){
     return (
         <div >
             <h1>I am a post</h1>
+
+            {/*Header*/}
+            <div>
+                <img src="" alt="" />
+                <p>{username}</p>
+                <DotsHorizontalIcon className="h-5"/>
+            </div>
+
+
+            {/*img*/}
+            {/*Buttons*/}
+            {/*Captions/*}
+            {/*Comments*/}
+            {/*input Box*/}
 
         </div>
     )
