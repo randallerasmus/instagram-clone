f21a3e5cb4a2cf506a3302659cf248467b7ce160
randall.erasmus <randallerasmus1@gmail.com>
2021-10-26 23:50:29 +0200

    Starting with the Post - grouped all the posts

--- components/Posts.js
+++ components/Posts.js
@@ -1,14 +1,42 @@
+import Post from "./Post";
+import * as posts from "tailwindcss";
+
+const DUMMY_DATA = [
+    {
+        id:123,
+        username:'randall',
+        userImg:'https://links.papareact.com/3ke',
+        img:'https://links.papareact.com/3ke',
+        caption:"SUBSCRIBE AND DESTROY THE LIKE " +
+            "BUTTON for the YT algorithm,"
+    },
+    {
+        id:123,
+        username:'joshaa',
+        userImg:'https://links.papareact.com/3ke',
+        img:'https://links.papareact.com/3ke',
+        caption:"SUBSCRIBE AND DESTROY THE LIKE " +
+            "BUTTON for the YT algorithm,"
+    },
+]
 
 function Posts(){
     return (
-        <div >
-            {/*Posts*/}
-            {/*Posts*/}
-            {/*Posts*/}
-            {/*Posts*/}
-            {/*Posts*/}
+        <div>
+            {posts.map((post) =>(
+            <Post key={post.id} id={post.id}
+                username={post.username}
+                userImg={post.userImg}
+                img={post.img}
+                caption={post.caption}/>
+                ))}
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+
 
         </div>
-    )
+    );
 }
-export default Posts
+export default Posts;
